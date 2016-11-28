21fe63bc9338daae3f3915903d9753659f9f1116
Aborn Jiang <loveaborn@foxmail.com>
2016-11-28 22:38:10 +0800

    获得当前光标下的单词

--- basic/text-example.el
+++ basic/text-example.el
@@ -1,1 +1,8 @@
 ;; text文本相当的函数
+
+(defun lebe-say-word (word)
+  "获得当前光标下的单词"
+  (interactive (list
+                (read-string (format "word (%s): " (thing-at-point 'word))
+                             nil nil (thing-at-point 'word))))
+  (message "The word is %s" word))
